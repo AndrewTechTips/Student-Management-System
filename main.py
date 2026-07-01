7701c69b538ee7df4e5fe79e94154e0ade52a98f
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-01 15:02:59 +0300

    implement data loading from SQLite database into the Student Management System GUI

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QMainWindow,
     QTableWidget,
+    QTableWidgetItem,
 )
+
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -28,10 +31,29 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
-        table = QTableWidget()
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+
+        self.table.setRowCount(0)
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(
+                    row_number, column_number, QTableWidgetItem(str(data))
+                )
+
+        connection.close()
 
 
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
