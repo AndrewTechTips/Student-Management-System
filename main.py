0698645370b52560ba24d30fce339cb3fef1352f
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-03 14:27:43 +0300

    refactor main.py to simplify imports and initialize MainWindow from ui module

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import sys
-
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtWidgets import (
-    QApplication,
-    QLineEdit,
-    QPushButton,
-    QMainWindow,
-    QTableWidget,
-    QTableWidgetItem,
-    QDialog,
-    QVBoxLayout,
-    QComboBox,
-    QToolBar,
-    QStatusBar,
-    QGridLayout,
-    QLabel,
-    QMessageBox,
-)
+from PyQt6.QtWidgets import QApplication
+from ui import MainWindow
 
 app = QApplication(sys.argv)
+
 main_window = MainWindow()
 main_window.show()
 main_window.load_data()
+
 sys.exit(app.exec())
